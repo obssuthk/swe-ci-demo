654bc9ec7e2aa32414d00b24300b580df7d0e983
Kevin Obssuth <90472692+obssuthk@users.noreply.github.com>
2022-03-30 14:57:08 -0400

    Update FizzBuzz.java
    updating some errors

--- src/main/java/com/demo/FizzBuzz.java
+++ src/main/java/com/demo/FizzBuzz.java
@@ -28,12 +28,12 @@ public class FizzBuzz
 	}
 	
 	String processNumbers (int[] nums)
-	{
-    String temp = "";
-    if(nums.length == 0)
-          {
-            return "invalid";
-          }
+{
+    		String temp = "";
+    		if(nums.length == 0)
+          	{
+           		 return "invalid";
+          	}
         for(int i = 0; i < nums.length-1; i++)
         {
           if(nums[i] == 0)
@@ -59,25 +59,26 @@ public class FizzBuzz
 		
 		
         }
-	if(nums[i] == 0)
+		
+	if(nums[nums.length - 1] == 0)
           {
             temp += String.valueOf(0);  
           }
-          else if(nums[i] % 15 == 0)
+          else if(nums[nums.length - 1] % 15 == 0)
           {
             temp += "FizzBuzz";
           }
-          else if(nums[i] % 5 == 0)
+          else if(nums[nums.length - 1] % 5 == 0)
           {
             temp += "Buzz";
           }
-          else if(nums[i] % 3 == 0)
+          else if(nums[nums.length - 1] % 3 == 0)
           {
             temp += "Fizz";
           }
           else
           {
-            temp += String.valueOf(nums[i]);
+            temp += String.valueOf(nums[nums.length - 1]);
           }
         return temp;
 	}
